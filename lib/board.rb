3f9b9fd6620f685b27be33eb1cfe5c9efd80453f
Daiane Bushey <daianebushey@gmail.com>
2018-04-12 21:43:58 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,14 @@
 class Board
   attr_accessor :cells
-  
+  # attr_reader :cells
+  #
+  # def cells=("X")
+  #   board.cells
+  # end
 
   def initialize
     reset!
+    @cells = Array.new(9)
   end
 
   def reset!
