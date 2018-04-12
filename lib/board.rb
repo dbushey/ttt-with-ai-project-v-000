1d751c0db3a42ebf8b39979612ad934c66aa9dd3
Daiane Bushey <daianebushey@gmail.com>
2018-04-12 21:49:00 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,19 +1,16 @@
 class Board
   attr_accessor :cells
-  # attr_reader :cells
-  #
-  # def cells=("X")
-  #   board.cells
-  # end
 
   def initialize
     reset!
-
   end
 
   def reset!
     @cells = Array.new(9, " ")
-    #board.cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def display
+
   end
 
 
