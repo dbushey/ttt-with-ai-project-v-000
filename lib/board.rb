3e55c469b223db38ef9569c2b88460717db2f468
Daiane Bushey <daianebushey@gmail.com>
2018-04-12 21:52:38 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -10,7 +10,11 @@ class Board
   end
 
   def display
-
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
 
